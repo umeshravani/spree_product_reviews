ebd4da50d6401a825a4e0b066fd2c1f2d9db7a6c
Umesh Ravani <umeshravani98@gmail.com>
2025-12-08 12:31:01 +0530

    Fix routes for product reviews and forbidden access

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,8 @@ Spree::Core::Engine.add_routes do
       resources :product_reviews, only: %i[index destroy edit update] do
         member do
           get :approve
-        get :disapprove
-          get '/forbidden', to: 'spree/home#forbidden', as: :forbidden
+          get :disapprove
+          get '/forbidden', to: 'spree/home#forbidden', as: :admin_forbidden
         end
       end
     end
@@ -15,5 +15,6 @@ Spree::Core::Engine.add_routes do
     resources :product_reviews, only: %i[index new create]
   end
 
+  # Keep the frontend route name as forbidden
   get '/forbidden', to: 'spree/home#forbidden', as: :forbidden
 end
