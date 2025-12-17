a179ef6933fa3276401aaddafcd84271795017e8
Umesh Ravani <umeshravani98@gmail.com>
2025-12-17 15:14:07 +0530

    Delete app directory for Fresh initialisation

--- app/controllers/spree/home_controller.rb
+++
@@ -1,7 +0,0 @@
-module Spree
-  class HomeController < Spree::StoreController
-    def forbidden
-      render plain: "You are not authorized to access this page.", status: :forbidden
-    end
-  end
-end
