5c11c4a89df06d65ecd87c23c7f5246bd242c46b
Umesh Ravani <umeshravani98@gmail.com>
2025-12-17 18:26:40 +0530

    Update install_generator.rb

--- lib/generators/spree_product_reviews/install/install_generator.rb
+++ lib/generators/spree_product_reviews/install/install_generator.rb
@@ -8,12 +8,36 @@ module SpreeProductReviews
       end
 
       def run_migrations
-        run_migrations = options[:migrate] || ['', 'y', 'Y'].include?(ask('Would you like to run the migrations now? [Y/n]'))
+        run_migrations = options[:migrate] || ['', 'y', 'Y'].include?(
+          ask('Would you like to run the migrations now? [Y/n]')
+        )
+
         if run_migrations
           run 'bin/rails db:migrate'
         else
           puts 'Skipping rails db:migrate, don\'t forget to run it!'
         end
+      end
+
+      def add_reviews_page_block
+        say_status :spree_product_reviews, "Adding Reviews block to Product Details sections", :green
+
+        # Load app environment explicitly (important for generators)
+        require Rails.root.join("config/environment")
+
+        Spree::PageSection
+          .where(type: "Spree::PageSections::ProductDetails")
+          .find_each do |section|
+
+            next if section.blocks.exists?(
+              type: "Spree::PageBlocks::Products::Reviews"
+            )
+
+            section.blocks.create!(
+              type: "Spree::PageBlocks::Products::Reviews",
+              position: section.blocks.maximum(:position).to_i + 1
+            )
+          end
       end
     end
   end
