f6e54ca48194e8134c196eff5ffcd2508beb00ee
Umesh Ravani <umeshravani98@gmail.com>
2025-12-17 17:06:13 +0530

    Update 20250501174531_add_review_info_to_products.rb

--- db/migrate/20250501174531_add_review_info_to_products.rb
+++ db/migrate/20250501174531_add_review_info_to_products.rb
@@ -1,7 +1,13 @@
 class AddReviewInfoToProducts < ActiveRecord::Migration[7.2]
   def change
-    add_column :spree_products, :average_rating, :decimal, precision: 7, scale: 5, default: 0.0, null: false
-    add_column :spree_products, :review_count, :integer, default: 0, null: false
+    unless column_exists?(:spree_products, :average_rating)
+      add_column :spree_products, :average_rating, :decimal,
+                 precision: 7, scale: 5, default: 0.0, null: false
+    end
+
+    unless column_exists?(:spree_products, :review_count)
+      add_column :spree_products, :review_count, :integer,
+                 default: 0, null: false
+    end
   end
 end
-
