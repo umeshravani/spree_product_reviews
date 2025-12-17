d59df97f4bfcb9fd2f17676453c6b481ffa2e695
Umesh Ravani <umeshravani98@gmail.com>
2025-12-17 16:53:26 +0530

    Delete config directory

--- config/initializers/spree_product_reviews.rb
+++
@@ -1,9 +0,0 @@
-Rails.application.config.after_initialize do
-  Rails.application.config.spree_admin.product_dropdown_partials << "spree_product_reviews/admin/product_reviews_dropdown"
-  Spree::Ability.register_ability(Spree::ProductReviewsAbility)
-  Spree::Ability.register_ability(Spree::ProductReviewsAbility)
-
-  Rails.application.config.spree.page_sections << Spree::PageSections::AddAReview
-  Rails.application.config.spree.page_blocks << Spree::PageBlocks::ProductReviewForm
-end
-
