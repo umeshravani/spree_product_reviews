570fb58b4bc20ac96d89c402d230256aa961c958
Umesh Ravani <umeshravani98@gmail.com>
2026-01-05 21:13:05 +0530

    Clean up engine.rb by removing comments and initializer
    Removed unused comments and initializer for configuration.

--- lib/spree_product_reviews/engine.rb
+++ lib/spree_product_reviews/engine.rb
@@ -4,13 +4,8 @@ module SpreeProductReviews
     isolate_namespace Spree
     engine_name 'spree_product_reviews'
 
-    # use rspec for tests
     config.generators do |g|
       g.test_framework :rspec
-    end
-
-    initializer 'spree_product_reviews.environment', before: :load_config_initializers do |_app|
-      SpreeProductReviews::Config = SpreeProductReviews::Configuration.new
     end
 
     def self.activate
