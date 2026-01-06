8645728bf46f22573393825a25a43d536f3ce282
Umesh Ravani <umeshravani98@gmail.com>
2026-01-07 00:19:24 +0530

    Disable image validation for ProductReview
    Remove image validation from Spree::ProductReview.

--- lib/spree_product_reviews/engine.rb
+++ lib/spree_product_reviews/engine.rb
@@ -15,5 +15,23 @@ module SpreeProductReviews
     end
 
     config.to_prepare(&method(:activate).to_proc)
+
+    config.after_initialize do
+      Rails.application.reloader.to_prepare do
+        if defined?(Spree::ProductReview)
+          Spree::ProductReview._validators.delete(:images)
+          Spree::ProductReview._validate_callbacks.delete_if do |callback|
+            filter = callback.filter
+            if filter.is_a?(Symbol) && filter.to_s.include?("image") 
+               true
+            elsif filter.respond_to?(:attributes) && filter.attributes.include?(:images)
+               true
+            else
+               false
+            end
+          end
+        end
+      end
+    end
   end
 end
