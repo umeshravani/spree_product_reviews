fa2cfb2524d935a81fedccde1a1f447b8d507d05
Umesh Ravani <umeshravani98@gmail.com>
2026-02-12 02:59:14 +0530

    Add member and collection routes for reviews

--- config/routes.rb
+++ config/routes.rb
@@ -2,22 +2,38 @@ Spree::Core::Engine.add_routes do
   namespace :admin do
     # GLOBAL REVIEWS (For the "Reviews" tab)
     resources :reviews do
+      # Single Item Actions
       member do
         get :approve
         get :disapprove
         post :attach_image 
         delete :purge_images
       end
+
+      # Bulk Actions (Multi-select)
+      collection do
+        put :bulk_approve
+        put :bulk_disapprove
+        delete :bulk_destroy
+      end
     end
 
     # NESTED REVIEWS (For the "Products" tab -> "Reviews")
     resources :products do
       resources :product_reviews, only: %i[index destroy edit update] do
+        # Single Item Actions
         member do
           get :approve
           get :disapprove
           post :attach_image
           delete :purge_images
+        end
+
+        # Bulk Actions (Multi-select)
+        collection do
+          put :bulk_approve
+          put :bulk_disapprove
+          delete :bulk_destroy
         end
       end
     end
