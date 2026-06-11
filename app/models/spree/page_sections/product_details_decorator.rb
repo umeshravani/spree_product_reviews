9d67ee73bc84f3186d4a88458d75785a927e0ebe
Umesh Ravani <umeshravani98@gmail.com>
2026-06-11 12:32:27 +0530

    Update product_details_decorator.rb

--- app/models/spree/page_sections/product_details_decorator.rb
+++ app/models/spree/page_sections/product_details_decorator.rb
@@ -1,13 +1,14 @@
-if defined?(Spree::PageSections::ProductDetails)
-  module Spree
-    module PageSections
-      module ProductDetailsDecorator
-        def available_blocks_to_add
-          super + [Spree::PageBlocks::Products::Reviews]
-        end
+module Spree
+  module PageSections
+    module ProductDetailsDecorator
+      def available_blocks_to_add
+        super + [Spree::PageBlocks::Products::Reviews]
       end
     end
   end
+end
 
+# Sits OUTSIDE the module definition
+if defined?(Spree::PageSections::ProductDetails)
   Spree::PageSections::ProductDetails.prepend(Spree::PageSections::ProductDetailsDecorator)
 end
